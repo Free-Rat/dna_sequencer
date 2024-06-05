f900a3b023cb1bb471f9ede934ec7fa9fca26806
Free-Rat <lawicki02@gmail.com>
2024-06-05 19:05:42 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake with shell for dna sequencer Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell {
+
+          shellHook = ''
+            echo "Hello in dna sequencer"
+          '';
+
+          packages = (with pkgs; [
+            gnumake
+            gcc
+            clang-tools_17
+            git
+          ]);
+        };
+    };
+}
